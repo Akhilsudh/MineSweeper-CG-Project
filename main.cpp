79450e0c4dffa63f1f6efa55713d35be76ff7468
Akhil Sudhakaran <akhil.sudh@gmail.com>
2017-04-05 21:01:11 +0530

    Minor changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include "drawer.hpp"
+#include "gameMechanics.hpp"
 #include <GL/glut.h>
+
+Game game;
 
 void display()
 {
@@ -13,13 +17,13 @@ int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(140, 140);
+    glutInitWindowSize(Game::WIDTH * Drawer::CELL_WIDTH, Game::HEIGHT * Drawer::CELL_HEIGHT);
     glutInitWindowPosition(100, 120);
-    glutCreateWindow("MineSweeper");
+    glutCreateWindow("MineSweeper-CG-Project");
     glClearColor(0, 0, 0, 1);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0, 140, 140, 0, -1.0, 1.0);
+    glOrtho(0, Game::WIDTH * Drawer::CELL_WIDTH, Game::HEIGHT * Drawer::CELL_HEIGHT, 0, -1.0, 1.0);
     glutDisplayFunc(display);
     glutMouseFunc(mouse); //Call the mouse function here
     glutMainLoop();

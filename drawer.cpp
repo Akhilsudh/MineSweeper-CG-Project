3ef5664c201cadf2ebe48fb6c303b7f0e8589bd7
Akhil Sudhakaran <akhil.sudh@gmail.com>
2017-04-05 19:15:38 +0530

    Flag draw function added

--- drawer.cpp
+++ drawer.cpp
@@ -17,7 +17,29 @@ void Drawer::drawMine(int x, int y) //Draw a Black Pentagon for a mine
 }
 
 void Drawer::drawFlag(int x, int y)
-{}
+{
+	//Draw the Flag that must be placed when clicking the right mouse button
+	glColor3f(0.8f, 0.8f, 0.8f);
+    glBegin(GL_QUADS);
+    glVertex2f(x * CELL_WIDTH, y * CELL_HEIGHT);
+    glVertex2f((x + 1) * CELL_WIDTH, y * CELL_HEIGHT);
+    glVertex2f((x + 1) * CELL_WIDTH, (y + 1) * CELL_HEIGHT);
+    glVertex2f(x * CELL_WIDTH, (y + 1) * CELL_HEIGHT);
+    glEnd();
+    //Draw the Black flag pole
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glBegin(GL_LINES);
+    glVertex2f(x * CELL_WIDTH + CELL_WIDTH / 2, y * CELL_HEIGHT + 3);
+    glVertex2f(x * CELL_WIDTH + CELL_WIDTH / 2, (y + 1) * CELL_HEIGHT - 3);
+    glEnd();
+    //Draw the red triangle of the flag
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(x * CELL_WIDTH + CELL_WIDTH / 2, y * CELL_HEIGHT + 3);
+    glVertex2f(x * CELL_WIDTH + CELL_WIDTH / 2 - 3, y * CELL_HEIGHT + 3 + 3);
+    glVertex2f(x * CELL_WIDTH + CELL_WIDTH / 2, y * CELL_HEIGHT + 3 + 3 + 3);
+    glEnd();
+}
 
 void Drawer::drawClosedField(int x, int y)
 {}

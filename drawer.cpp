7af74bd57735fc9253580713756c0fec2f926b63
Akhil Sudhakaran <akhil.sudh@gmail.com>
2017-04-05 19:14:57 +0530

    Draw Mine Object function done

--- drawer.cpp
+++ drawer.cpp
@@ -4,8 +4,17 @@
 #include <cmath>
 #include <iostream>
 
-void Drawer::drawMine(int x, int y)
-{}
+void Drawer::drawMine(int x, int y) //Draw a Black Pentagon for a mine
+{
+	drawOpenedField(x, y); //Place a mine on the opened field
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glBegin(GL_POLYGON); 
+    for (int i = 0; i < 5; ++i)
+    {    
+    	glVertex2f(x * CELL_WIDTH + CELL_WIDTH / 2 + 5.0f * cos(2 * 3.1415926 * i / 5), y * CELL_HEIGHT + CELL_HEIGHT / 2 + 5.0f * sin(2 * 3.1415926 * i / 5));
+    }
+    glEnd();
+}
 
 void Drawer::drawFlag(int x, int y)
 {}
